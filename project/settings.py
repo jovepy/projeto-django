68b57bf8377e168ff5eb20c32bac98b7f38f7d7e
Rodrigo Jovê <datajove@gmail.com>
2022-06-07 13:30:12 -0300

    AGR VAI HEROKU

--- project/settings.py
+++ project/settings.py
@@ -87,13 +87,18 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+"""
+
+DATABASES = {
+    
 }
 
 
