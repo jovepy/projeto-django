ca7181fff7f1fe79dd46bb77e2191970cc3735f1
Rodrigo Jovê <datajove@gmail.com>
2022-06-07 13:18:02 -0300

    ok

--- project/settings.py
+++ project/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'NAME': 'dbtest', 
         'USER': 'postgres', 
         'PASSWORD': '1234',
-        'HOST': '127.0.0.1', 
+        'HOST': 'localhost', 
         'PORT': '5432',
     }
 }
